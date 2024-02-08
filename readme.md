05c6c61a920f13615538bf877f1e4912d25f62b6
nice_sprite <cox.tristan.tc@gmail.com>
2024-02-07 23:52:26 -0600

    added readme instructions

---
+++ readme.md
@@ -0,0 +1,6 @@
+# Get started
+- Git clone the repo
+- Create a virtual environment with `python -m venv ./venv`
+- Run `./venv/bin/pip install -r requirements.txt` to install the required packages in the virtual environment
+- `run.sh` executes `treebased.py` in the virtual environment
+
--- treebased.py
+++ treebased.py
@@ -113,3 +113,100 @@ plt.xlabel('y_pred')
 plt.ylabel('y_true')
 plt.title('catboost confusion matrix')
 plt.savefig('catboost_confusion_matrix.png')
+
+
+
+
+
+
+
+## Proposed ensemble model: Leader Class and Confidence Decision Ensemble (LCCDE)
+
+### Find the best-performing (leading) model for each type of attack among the three ML models
+model=[]
+for i in range(len(lg_f1)):
+    if max(lg_f1[i],xg_f1[i],cb_f1[i]) == lg_f1[i]:
+        model.append(lg)
+    elif max(lg_f1[i],xg_f1[i],cb_f1[i]) == xg_f1[i]:
+        model.append(xg)
+    else:
+        model.append(cb)
+
+print(model)
+
+
+def LCCDE(X_test, y_test, m1, m2, m3):
+    i = 0
+    t = []
+    m = []
+    yt = []
+    yp = []
+    l = []
+    pred_l = []
+    pro_l = []
+
+    # For each class (normal or a type of attack), find the leader model
+    for xi, yi in stream.iter_pandas(X_test, y_test):
+
+        xi2=np.array(list(xi.values()))
+        y_pred1 = m1.predict(xi2.reshape(1, -1))      # model 1 (LightGBM) makes a prediction on text sample xi
+        y_pred1 = int(y_pred1[0])
+        y_pred2 = m2.predict(xi2.reshape(1, -1))      # model 2 (XGBoost) makes a prediction on text sample xi
+        y_pred2 = int(y_pred2[0])
+        y_pred3 = m3.predict(xi2.reshape(1, -1))      # model 3 (Catboost) makes a prediction on text sample xi
+        y_pred3 = int(y_pred3[0])
+
+        p1 = m1.predict_proba(xi2.reshape(1, -1))     # The prediction probability (confidence) list of model 1 
+        p2 = m2.predict_proba(xi2.reshape(1, -1))     # The prediction probability (confidence) list of model 2  
+        p3 = m3.predict_proba(xi2.reshape(1, -1))     # The prediction probability (confidence) list of model 3  
+
+        # Find the highest prediction probability among all classes for each ML model
+        y_pred_p1 = np.max(p1)
+        y_pred_p2 = np.max(p2)
+        y_pred_p3 = np.max(p3)
+
+        if y_pred1 == y_pred2 == y_pred3: # If the predicted classes of all the three models are the same
+            y_pred = y_pred1 # Use this predicted class as the final predicted class
+
+        elif y_pred1 != y_pred2 != y_pred3: # If the predicted classes of all the three models are different
+            # For each prediction model, check if the predicted class’s original ML model is the same as its leader model
+            if model[y_pred1]==m1: # If they are the same and the leading model is model 1 (LightGBM)
+                l.append(m1)
+                pred_l.append(y_pred1) # Save the predicted class
+                pro_l.append(y_pred_p1) # Save the confidence
+
+            if model[y_pred2]==m2: # If they are the same and the leading model is model 2 (XGBoost)
+                l.append(m2)
+                pred_l.append(y_pred2)
+                pro_l.append(y_pred_p2)
+
+            if model[y_pred3]==m3: # If they are the same and the leading model is model 3 (CatBoost)
+                l.append(m3)
+                pred_l.append(y_pred3)
+                pro_l.append(y_pred_p3)
+
+            if len(l)==0: # Avoid empty probability list
+                pro_l=[y_pred_p1,y_pred_p2,y_pred_p3]
+
+            elif len(l)==1: # If only one pair of the original model and the leader model for each predicted class is the same
+                y_pred=pred_l[0] # Use the predicted class of the leader model as the final prediction class
+
+            else: # If no pair or multiple pairs of the original prediction model and the leader model for each predicted class are the same
+                max_p = max(pro_l) # Find the highest confidence
+                
+                # Use the predicted class with the highest confidence as the final prediction class
+                if max_p == y_pred_p1:
+                    y_pred = y_pred1
+                elif max_p == y_pred_p2:
+                    y_pred = y_pred2
+                else:
+                    y_pred = y_pred3  
+        
+        else: # If two predicted classes are the same and the other one is different
+            n = mode([y_pred1,y_pred2,y_pred3]) # Find the predicted class with the majority vote
+            y_pred = model[n].predict(xi2.reshape(1, -1)) # Use the predicted class of the leader model as the final prediction class
+            y_pred = int(y_pred[0]) 
+
+        yt.append(yi)
+        yp.append(y_pred) # Save the predicted classes for all tested samples
+        return yt, yp
